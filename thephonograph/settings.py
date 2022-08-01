6031a57a5201f8f1802744f217bb8cb1df528a26
Elisabetta <emaspero@hotmail.com>
2022-08-01 21:04:26 +0100

    Update

--- thephonograph/settings.py
+++ thephonograph/settings.py
@@ -37,7 +37,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u2q@wa8o2#a(w@g&*f9k&lk)hakmr@y_zxo4-!qz&m(0=2(mg-'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
